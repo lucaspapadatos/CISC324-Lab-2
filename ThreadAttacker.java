9e1e9d9d9476ac6fdfaff9a3fee0dfea34e84a88
Lucas Papadatos <lucaspapadatos@gmail.com>
2022-10-02 22:11:38 -0400

    finished 1/4

--- ThreadAttacker.java
+++ ThreadAttacker.java
@@ -3,7 +3,6 @@
 //starts its execution
 
 public class ThreadAttacker {
-
 	//This is the challenge value, you can modify the value if you want
 	public static String challenge = "challenge_sequence";
 	//This is the password. It is here to help us compute the correct response that corresponds to the challenge
@@ -13,7 +12,10 @@ public class ThreadAttacker {
 	//This is the variable that will be used by the thread to inform each other that the password has been cracked
 	//For instance, if a thread cracks the password, it update the value of this variable to true
 	public static boolean found = false;
-
+	//This is the variable that holds all letters of the alphabet for comparison
+	public static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	//This is the number of attempts one thread has made to compare has
+	public static int attempts = 0;
 	//The main method, here starts the execution	
 	public static void main(String[] args) 
 	{
@@ -26,14 +28,10 @@ public class ThreadAttacker {
 		ThreadBots t_1 = new ThreadBots(1,'i');
 		ThreadBots t_2 = new ThreadBots(2, 't');
 		ThreadBots t_3 = new ThreadBots(3, 'v');
-				
 		//Thread trigging (starting the threads)
-		t_1.start();
-		t_2.start();
+		//t_1.start();
+		//t_2.start();
 		t_3.start();
-		
-		
-		
 	}
 
 }
--- ThreadBots.java
+++ ThreadBots.java
@@ -1,12 +1,12 @@
+import java.text.BreakIterator;
+
 //The content of this file defines a Java class named 'ThreadBot' 
 //This class inherits from the predefined Java class Thread.
 
 public class ThreadBots extends Thread
 {
-
 	int Identity; //This integer variable will be the thread identifier
-        char init_char;//This character will be used by each thread as the first letter in the password
-
+  	char init_char;//This character will be used by each thread as the first letter in the password
 
 	//Here we redefine the default constructor of this class.
 	//By default it has no arguments, but in this example
@@ -19,11 +19,48 @@ public class ThreadBots extends Thread
 		init_char = c;
 	}
 	
+	public static int passToHash(String s) {
+		String challenge = ThreadAttacker.challenge;
+		String tempx = s + challenge; // add challenge string to password attempt
+		int r_ = tempx.hashCode(); // create new hash code
+		return r_;
+	}
+
+	public static void compareHash(int r_) {
+		//Returns true if both hash values are the same
+		int r = ThreadAttacker.captured;
+		if (r == r_) {
+			ThreadAttacker.found = true;
+		}
+		else {
+			ThreadAttacker.found = false;
+		}
+	}
+
+	public static void crack(StringBuilder sb, int n) {
+		if (ThreadAttacker.found) return;
+		if (n == sb.length()) {
+			int r = passToHash(sb.toString());
+			compareHash(r);
+			return;
+		}
+		for (int i = 0; i < ThreadAttacker.alphabet.length; i++) {
+			char letter = ThreadAttacker.alphabet[i];
+			sb.setCharAt(n, letter);
+			System.out.println(sb);
+			ThreadAttacker.attempts++;
+			crack(sb, n + 1);
+		}
+	}
+
 	public void run()
 	{
-		
-		//Here is where you write the code that should crack the password
-					
+		System.out.println("Thread ["+init_char+"] initiating...");
+		StringBuilder pass = new StringBuilder(init_char+"");
+		System.out.println(pass);
+		pass.setLength(5);
+		crack(pass, 1);
+		System.out.println(ThreadAttacker.attempts);
 	}
 
 }
